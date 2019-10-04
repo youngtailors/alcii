c7f5fabb8a8580e665bac40e304d18ccf894cf34
thong <thong@thongs-MacBook-Pro.local>
2019-10-04 11:50:56 +0700

    feat: update folder struture

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -42,19 +42,7 @@ module.exports = {
                             {
                                 title: '§2. PHƯƠNG PHÁP SINH (GENERATION)',
                                 path:
-                                    '/algorithm-and-data-structure/chap-one/lesson-two/lessontwo',
-                                children: [
-                                    {
-                                        title: 'Thứ tự từ điển',
-                                        path:
-                                            '/algorithm-and-data-structure/chap-one/lesson-two/dictionaryorder',
-                                    },
-                                    {
-                                        title: 'SINH CÁC DÃY NHỊ PHÂN ĐỘ DÀI N',
-                                        path:
-                                            '/algorithm-and-data-structure/chap-one/lesson-two/binaryarraygeneration',
-                                    },
-                                ],
+                                    '/algorithm-and-data-structure/chap-one/lessontwo',
                             },
                         ],
                     },
